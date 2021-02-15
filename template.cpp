e8bfa0c0d9e82a212fb36fd56ce122baf3a56437
Abhimanyu Maurya <aerma7309@gmail.com>
2021-02-15 23:59:44 +0530

    template.cpp

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int FastIO = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+void solve()
+{
+}
+
+signed main()
+{
+    int t, tc = 1;
+    cin >> t;
+    while (t--)
+    {
+        cout << "Case #" << tc++ << ": ";
+        solve();
+    }
+    return 0;
+}
